805718850ae713c9c423e2f23c8cd4ed3489f626
invisibleSascha <sascha@invisibleroom.com>
2021-06-22 10:20:48 +0200

    https

--- server/Classes/Socket.js
+++ server/Classes/Socket.js
@@ -1,9 +1,39 @@
 var port = 3333;
-var io = require('socket.io')(port,{ 
-  cors: {
-    origin: '*',
-  }
+var fs = require( 'fs' );
+var app = require('express')();
+var path = require("path");
+
+var keyFile = fs.readFileSync( path.resolve(__dirname, "../ssl/server.key") );
+var certFile = fs.readFileSync( path.resolve(__dirname, "../ssl/server.cert") );
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+
+var https        = require('https');
+var server = https.createServer({
+    key: keyFile,
+    cert: certFile,
+    ca: certFile,
+    requestCert: false,
+    rejectUnauthorized: false
+},app);
+
+server.listen(port,'0.0.0.0');
+
+
+
+var io = require('socket.io').listen(server,{
+  secure : true
 });
+
+//io.set('transports', ['websocket']);
+
+// port,{ 
+//   secure: true,
+//   cors: {
+//     origin: '*',
+//   }
+// });
 
 class Socket{
   
